1121baafa0a9e70480cabb5d61ce0fef76428ab5
Ezra <ezraw@junietta.com>
2025-02-20 23:07:43 -0500

    Add static prerendering

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,3 @@
-// export const prerender = true / false
-export const prerender = 'auto';
+
+export const prerender = true; // Use `true` for static prerendering, `false` for SPA (?)
+// export const prerender = 'auto';
