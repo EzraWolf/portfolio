4e5acbaa652b17bee536147f8095e687b9b33ef0
Ezra Wolf <wolfez@mail.uc.edu>
2026-05-01 02:41:14 -0400

    Remove text balance

--- src/lib/components/SplitParagraph.svelte
+++ src/lib/components/SplitParagraph.svelte
@@ -5,6 +5,6 @@
 </script>
 
 <section class="grid grid-cols-1 gap-8 sm:grid-cols-2">
-	<p class="text-justify text-balance">{@render left()}</p>
-	<p class="text-justify text-balance">{@render right()}</p>
+	<p class="text-justify">{@render left()}</p>
+	<p class="text-justify">{@render right()}</p>
 </section>
